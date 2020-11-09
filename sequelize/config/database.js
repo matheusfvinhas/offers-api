be9892198f9a3795d61a016083041ad1bbfb3263
Matheus Vinhas <matheusfvinhas@gmail.com>
2020-11-09 08:57:35 -0300

    sqlite default

--- sequelize/config/database.js
+++ sequelize/config/database.js
@@ -1,21 +1,21 @@
-module.exports = {
-    development: {
-        url: process.env.POSTGRES_URL,
-        dialect: 'postgres',
-    },
-    production: {
-        url: process.env.POSTGRES_URL,
-        dialect: 'postgres',
-    },
-};
-
 // module.exports = {
 //     development: {
-//         dialect: 'sqlite',
-//         storage: './database.sqlite3',
+//         url: process.env.POSTGRES_URL,
+//         dialect: 'postgres',
 //     },
 //     production: {
-//         dialect: 'sqlite',
-//         storage: './database.sqlite3',
+//         url: process.env.POSTGRES_URL,
+//         dialect: 'postgres',
 //     },
 // };
+
+module.exports = {
+    development: {
+        dialect: 'sqlite',
+        storage: './database.sqlite3',
+    },
+    production: {
+        dialect: 'sqlite',
+        storage: './database.sqlite3',
+    },
+};
--- src/config/database/index.ts
+++ src/config/database/index.ts
@@ -9,18 +9,18 @@ class Database {
 
     async init(): Promise<void> {
         try {
-            this.connection = new Sequelize.Sequelize(process.env.POSTGRES_URL, {
-                dialect: 'postgres',
-                dialectOptions: {
-                    useUTC: false,
-                    dateStrings: true,
-                },
-            });
-
-            // this.connection = new Sequelize.Sequelize({
-            //     dialect: 'sqlite',
-            //     storage: './database.sqlite3',
+            // this.connection = new Sequelize.Sequelize(process.env.POSTGRES_URL, {
+            //     dialect: 'postgres',
+            //     dialectOptions: {
+            //         useUTC: false,
+            //         dateStrings: true,
+            //     },
             // });
+
+            this.connection = new Sequelize.Sequelize({
+                dialect: 'sqlite',
+                storage: './database.sqlite3',
+            });
 
             await this.connection.authenticate();
 
