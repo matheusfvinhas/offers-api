e73638ae410c05d0db55963e1e436fee7fe3b048
Matheus Vinhas <matheusfvinhas@gmail.com>
2020-11-09 21:05:23 -0300

    added parameters to queries

--- src/controllers/offer.controller.ts
+++ src/controllers/offer.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { Op } from 'sequelize';
 import Campus from '../models/campus.model';
 import Course from '../models/course.model';
 import Offer from '../models/offer.model';
@@ -9,6 +10,7 @@ class OfferController {
     public async index(req: Request, res: Response): Promise<void> {
         try {
             const order = ((req.query.order as string) || '').toUpperCase();
+            const { university, course, kind, level, shift, city } = req.query;
 
             const offers = await Offer.findAll({
                 order: [['priceWithDiscount', order === 'ASC' || order === 'DESC' ? order : 'ASC']],
@@ -16,14 +18,41 @@ class OfferController {
                     {
                         model: Course,
                         as: 'course',
+                        required: true,
+                        where: {
+                            name: {
+                                [Op.iLike]: `%${course || ''}%`,
+                            },
+                            kind: {
+                                [Op.iLike]: `%${kind || ''}%`,
+                            },
+                            level: {
+                                [Op.iLike]: `%${level || ''}%`,
+                            },
+                            shift: {
+                                [Op.iLike]: `%${shift || ''}%`,
+                            },
+                        },
                         include: [
                             {
                                 model: Campus,
                                 as: 'campus',
+                                required: true,
+                                where: {
+                                    city: {
+                                        [Op.iLike]: `%${city || ''}%`,
+                                    },
+                                },
                                 include: [
                                     {
                                         model: University,
                                         as: 'university',
+                                        required: true,
+                                        where: {
+                                            name: {
+                                                [Op.iLike]: `%${university || ''}%`,
+                                            },
+                                        },
                                     },
                                 ],
                             },
